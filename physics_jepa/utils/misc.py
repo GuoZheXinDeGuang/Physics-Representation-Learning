79c30f7db25c1011f33c3373164d4140f708e4e0
GuoZheXinDeGuang <daniel_liu356@163.com>
2026-04-26 21:52:07 -0400

    organize the code in a clean way

--- physics_jepa/utils/misc.py
+++
@@ -1,3 +0,0 @@
-def distprint(x, local_rank=None):
-    if local_rank == 0:
-        print(x, flush=True)
--- scripts/run_finetune_disco.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source "$(dirname "$0")/env_setup.sh"
-
-# Pass the data directory path as $1
-python -m physics_jepa.baselines.disco --data_path $1
